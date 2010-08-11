73cc23e54cca8c3e1de4a10241f9847cabf23661
nsn <nsn@nightspawn.com>
2010-08-11 07:12:43 +0200

    collision

--- shmup/shmup.js
+++ shmup/shmup.js
@@ -1,59 +1,62 @@
+//global
+w = 640;
+h = 480;
+lives = 3;
+waves = 0;
+waven = 0;
+waveo = new vec(0,0);
+wavev = 50;
+mx = 0;
+my = 0;
+keys = new Array();
+// canvas element
+C = document.getElementById('c');
+C.width = w;
+C.height = h;
+C.addEventListener('mousemove', function(e) {
+    mx = e.clientX;
+    my = e.clientY;
+}, false);
+// context
+c = C.getContext('2d');
+// classes
+function length(t) {
+    return (Math.sqrt(t.x*t.x+t.y*t.y));
+}
+function norm(n) {
+    mul(n,1/length(n));
+}
+function mul(v,d) {
+    v.x = v.x*d;
+    v.y = v.y*d;
+}
+function sub(s1,s2) {
+    return new vec(s1.x-s2.x,s1.y-s2.y);
+}
 function vec(x,y) {
     this.x = x;
     this.y = y;
 }
-function va(d) {
-    len=d.length/2;
-    rv = new Array(len);
-    for (i=0;i<len;i++) {
-        rv[i]=new vec(d[i*2],d[i*2+1]);
-    }
-    return rv;
-}
-function obj(p,d) {
+function obj(p) {
     this.pos = p;
-    this.dir = d;
+    this.dir = new vec(0,-1);
+    this.bound = 8;
+    this.con = false;
     this.scale = new vec(1,1);
     this.color = '#fff';
-
-    this.prep = function(c) {
-        c.translate(this.pos.x, this.pos.y);
-        c.scale(this.scale, this.scale);
-        c.shadowOffsetX = 0;
-        c.shadowOffsetY = 0;
-        c.shadowColor = 'yellow';
-        c.shadowBlur = 2;
-        c.lineWidth=1;
-        c.strokeStyle=this.color;
+    this.speed = 10;
+    this.mov = function() {
+        norm(this.dir);
+        x = this.pos.x + this.dir.x*this.speed;
+        y = this.pos.y + this.dir.y*this.speed;
+        b = this.bound;
+        this.pos.x = (this.con&&(x-b<0||x+b>w))?this.pos.x:x;
+        this.pos.y = (this.con&&(y-b<0||y+b>h))?this.pos.y:y;
     }
 }
-function poly(p,d) {
-    obj.call(this, p,d);
-    this.verts = new Array();
-
-    this.draw = function(c) {
-        this.prep(c);
-
-        c.beginPath();
-        c.moveTo(this.verts[0].x, this.verts[0].y);
-
-        len = this.verts.length;
-        for (i=1;i<len;i++) {
-            v = this.verts[i];
-            c.lineTo(v.x, v.y);
-        }
-        for (i=len-1;i>=0;i--) {
-            v = this.verts[i];
-            c.lineTo(-v.x, v.y);
-        }
-
-        c.closePath();
-        c.stroke();
-    }
-}
-//idea: make all polygons symmetrical -> saves half the vertices
-function player(p,d) {
-    poly.call(this, p,d);
+function user(p) {
+    obj.call(this, p);
+    this.con = true;
     this.verts = va(new Array(
         4,-4,
         12,0,
@@ -64,27 +67,177 @@ function player(p,d) {
         0,6
     ));
 }
+function enemy(l) {
+    obj.call(this, l);
+    this.speed = 7;
+    this.upd = function() {
+        this.dir = sub(player.pos,this.pos);
+        this.mov();
+        if (col(this, player)) 
+            hit();
+    }
+}
+function shuttle(p) {
+    enemy.call(this, p);
+    this.color='red';
+    this.verts = va(new Array(
+        0,-8,
+        2,0,
+        4,-4,
+        8,-4,
+        4,4,
+        0,8
+    ));
+}
+function shot() {
+    obj.call(this, new vec(player.pos.x, player.pos.y));
+    this.speed=15;
+    this.color='#0ff';
+    this.dir = sub(new vec(mx,my), player.pos);
+    this.verts = va(new Array(
+        0,-8,
+        4,0,
+        0,4
+    ));
+}
+// helpers
+function va(d) {
+    l=d.length/2;
+    rv = new Array(l);
+    for (i=0;i<l;i++)
+        rv[i]=new vec(d[i*2],d[i*2+1]);
+    return rv;
+}
+function col(a,b) {
+    return length(sub(a.pos,b.pos))<(a.bound+b.bound);
+}
+// drawing
+function prep(c,o) {
+    r = o.dir;
+    c.transform(r.y,-r.x,r.x,r.y,o.pos.x,o.pos.y);
+    c.scale(o.scale.x, o.scale.y);
+    c.shadowOffsetX = 0;
+    c.shadowOffsetY = 0;
+    c.shadowColor = o.color;
+    c.shadowBlur = 1;
+    c.lineWidth=1;
+    c.strokeStyle=o.color;
+}
+function draw(c,g) {
+    c.save();
+    prep(c,g);
+    c.beginPath();
+    pv = g.verts;
+    c.moveTo(pv[0].x, pv[0].y);
 
-function shmup() {
-    w = 640;
-    h = 480;
-    C = document.getElementById('c');
-    C.width = w;
-    C.height = h;
-    c = C.getContext("2d");
+    l = pv.length;
+    for (j=1;j<l;j++) {
+        v = pv[j];
+        c.lineTo(v.x, v.y);
+    }
+    for (j=l-1;j>0;j--) {
+        v = pv[j-1];
+        c.lineTo(-v.x, v.y);
+    }
 
+    c.closePath();
+    c.stroke();
 
-    p = new player(new vec(100, 100), new vec(0,0));
-
-    c.fillRect(0,0,w,h);
-
-    p.draw(c);
-
+    c.restore();
 
 }
+// game loop
+function loop() {
+    // clear
+    c.fillRect(0,0,w,h);
+    // update player
+    d=player.dir;
+    x = y = 0;
+    y += keys[87]?-1:0;
+    y += keys[83]?1:0;
+    x += keys[65]?-1:0;
+    x += keys[68]?1:0;
+    if (x!=0||y!=0) {
+        d.x=x;
+        d.y=y;
+    }
+    player.mov();   
+    // draw enemies
+    for (i=0;i<enemies.length;i++) {
+        e = enemies[i];
+        e.upd();
+        draw(c,e);
+    }
+    // draw player
+    draw(c,player);
+    for (i=0;i<shots.length;i++) {
+        s = shots[i];
+        s.mov();
+        draw(c,s);
+    }
+    // shots
+    // draw hud
+    hp = new user(new vec(12,12));
+    hp.scale = (new vec(.5,.5));
+    hp.color = '#0f0';
+    draw(c,hp);
+    c.strokeStyle='#0f0';
+    c.strokeText("x"+lives, 25, 13);
+
+    setTimeout(loop, 50);
+}
+function wp(k) {
+    return k*w + (-1+k*2)*Math.random()*wavev;
+}
+function wave() {
+    if (waven<=waves) {
+        x = wp(waveo.x);
+        y = wp(waveo.y);
+        enemies.push(new shuttle(new vec(wp(waveo.x), wp(waveo.y))));
+        waven++;
+        setTimeout(wave,200);    
+    } else {
+        waven = 0;
+        waveo.x = Math.random()>.5?1:0;
+        waveo.y = Math.random()>.5?1:0;
+        waves+=2;
+        setTimeout(wave,3000);    
+    }
+}
+function shoot() {
+    log('asasd');
+    shots.push(new shot()); 
+    setTimeout(shoot, 250);
+}
+function hit() {
+    lives--;
+    if (lives < 0)
+        alert("over!");
+    init();
+}
+function init() {
+    // objects
+    player = new user(new vec(100, 150));
+    enemies = new Array();
+    shots = new Array();
+    shoot();
+}
+// player hit
+
+// main
+
+window.onkeydown = function(e) {
+    keys[e.keyCode] = true;
+}
+window.onkeyup = function(e) {
+    keys[e.keyCode] = false;
+}
+init();
+loop();
+wave();
+
 //TODO: remove this and all references
 function log(msg) {
     console.log(msg);
 }
-shmup();
 //http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html
